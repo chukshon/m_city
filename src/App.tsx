f39008ac17d564a9d786f709df03b3dbae300810
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-08-26 23:50:02 +0100

    routing manually done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './pages/home/Home'
+import Matches from './pages/matches/Matches'
+import PlayerInfo from './pages/playerInfo/PlayerInfo'
+import Team from './pages/team/Team'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/matches' element={<Matches />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/player_info/:id' element={<PlayerInfo />} />
+        <Route path='*' element={<h2>Does not exist</h2>} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
