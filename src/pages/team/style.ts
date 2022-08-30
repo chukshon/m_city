7a82eda402c07b2f62a66214a390733701b92774
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-08-30 23:13:47 +0100

    player card responsiveness started

--- src/pages/team/style.ts
+++ src/pages/team/style.ts
@@ -13,5 +13,16 @@ export const Container = styled.div`
 export const CategoryGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 320px);
+  justify-content: center;
   gap: 20px;
+  @media (min-width: 500px) {
+    justify-content: normal;
+  }
+  @media (min-width: 720px) {
+    grid-template-columns: repeat(2, 320px);
+    justify-content: normal;
+  }
+  @media (min-width: 1110px) {
+    grid-template-columns: repeat(3, 320px);
+  }
 `
