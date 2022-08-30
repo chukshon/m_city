9219e0096fe56d4cac0c7c3049436af88ebbf5b1
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-08-30 21:32:20 +0100

    win a jersey section done

---
+++ src/pages/home/containers/win_a_jersey/WinJersey.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WinJersey = () => {
+  return <div>Win Jersey</div>
+}
+
+export default WinJersey
---
+++ src/pages/home/containers/win_a_jersey/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div``
+
+export const Container = styled.div``
+
+export const TopSection = styled.div``
+
+export const EnterEmailSection = styled.div``
